130aeabd771c5f19525c61bd76d877fced4a2dbb
falkenbach <alexey.udovydchenko@gmail.com>
2019-11-07 12:43:29 +0700

    Parsing for companies

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ const Apify = require('apify');
 const requestPromise = require('request-promise');
 const cheerio = require('cheerio');
 const { URL } = require('url');
+const Entities = require('html-entities').AllHtmlEntities;
 
 const { log } = Apify.utils;
 const DOMAIN = 'https://www.glassdoor.com';
 
 Apify.main(async () => {
+    const entities = new Entities();
     const input = await Apify.getInput();
     const proxy = Apify.getApifyProxyUrl();
     const options = {
@@ -22,7 +24,7 @@ Apify.main(async () => {
             Connection: 'keep-alive',
         },
         gzip: true,
-        proxy
+        proxy: '',
     };
     const optionsCheerio = {
         transform: (body) => {
@@ -35,9 +37,11 @@ Apify.main(async () => {
         throw new Error('INPUT must contain query');
     }
     // if no maxResults specified then parse this amount from first search page
-    let maxResults = parseInt(input.maxResults, 10);
+    let { maxResults } = input;
     if (!maxResults) {
         maxResults = -1;
+    } else if (typeof maxResults === 'string') {
+        maxResults = parseInt(input.maxResults, 10);
     }
 
     if (typeof input.location === 'string') {
@@ -87,14 +91,31 @@ Apify.main(async () => {
         return {
             id: $(el).data('id'),
             employerName: $('div.jobInfoItem.jobEmpolyerName', el).text(),
-            rating: parseFloat($('span.compactStars', el).text()),
-            title: $('a', el).last().text(),
-            location: $('span.subtle.loc', el).text(), // div.jobInfoItem.empLoc includes tooltips like "hot" or "easy hire"
-            url: $('a', el).attr('href'),
+            employerRating: parseFloat($('span.compactStars', el).text()),
+            jobTitle: $('a', el).last().text(),
+            jobLocation: $('span.subtle.loc', el).text(), // div.jobInfoItem.empLoc includes tooltips like "hot" or "easy hire"
+            url: DOMAIN + $('a', el).attr('href'),
             jobDetails: '',
             companyDetails: '',
             salary: $('span.salaryText', el).text().trim(),
             benefits: '',
+        };
+    };
+
+    const mapReviewListItem = (i, el) => {
+        // original urls like /Overview/Working-at-Web-com-EI_IE12965.11,18.htm
+        // should be changed to company jobs urls like /Jobs/Web-com-Jobs-E12965.htm
+        const url1 = $('div.margBotXs a', el).attr('href');
+        let url2 = url1.replace('Overview/Working-at-', 'Jobs/');
+        const indA = url2.indexOf('-EI_IE');
+        const indB = url2.indexOf('.', indA);
+        url2 = url2.substr(0, indB);
+        url2 = `${url2.replace('-EI_IE', '-Jobs-E')}.htm`;
+        return {
+            id: $(el).data('emp-id'),
+            employerName: $('div.margBotXs a', el).text().trim(),
+            employerRating: parseFloat($('span.bigRating.strong.margRtSm.h1', el).text()),
+            url: url2,
         };
     };
 
@@ -103,47 +124,159 @@ Apify.main(async () => {
     * then crawl subpages to get details
     */
     const searchResults = [];
+    const reviewResults = [];
     // if no limit for results, then parse it from the initial search
     let maximumResults = maxResults > 0 ? maxResults : -1;
-    do {
-        try {
-            const searchUrl = new URL(nextPageUrl, DOMAIN);
-            log.info(`GET ${searchUrl}`);
-            $ = await requestPromise({
-                uri: searchUrl,
-                ...optionsCheerio,
-            });
-            if (maximumResults < 0) {
-                const cntStr = $('p.jobsCount').text().replace(',', '');
-                maximumResults = parseInt(cntStr, 10);
-                if (!(maximumResults > 0)) {
-                    throw new Error(`Failed to parse jobsCount from ${cntStr}`);
-                }
-                log.info(`Parsed maximumResults = ${maximumResults}`);
-            }
-            rawdata = $('li.jl');
-            json = rawdata
-                .map(mapJobListItem)
-                .get();
-        } catch (error) {
-            if (error.statusCode === 504 && limitRetries < 5) {
-                log(' - Encountered rate limit, waiting 3 seconds');
-                await Apify.utils.sleep(3000);
-                limitRetries++;
-                continue; // eslint-disable-line
-            } else {
-                // Rethrow non rate-limit errors or if we are stuck
-                throw error;
-            }
-        }
 
-        const itemsToSave = json.slice(0, maximumResults - savedItems);
-        searchResults.push(...itemsToSave);
-        savedItems += itemsToSave.length;
-        nextPageUrl = $('li.next a', '#FooterPageNav').attr('href');
-        log.info(`Page ${page}: Found ${itemsToSave.length} items, next page: ${nextPageUrl}`);
-        page++;
-    } while (nextPageUrl && savedItems < maximumResults);
+    if (input.category === 'Jobs') {
+        do {
+            try {
+                const searchUrl = new URL(nextPageUrl, DOMAIN);
+                log.info(`GET ${searchUrl}`);
+                $ = await requestPromise({
+                    uri: searchUrl,
+                    ...optionsCheerio,
+                });
+                if (maximumResults < 0) {
+                    const cntStr = $('p.jobsCount').text().replace(',', '');
+                    maximumResults = parseInt(cntStr, 10);
+                    if (!(maximumResults > 0)) {
+                        throw new Error(`Failed to parse jobsCount from ${cntStr}`);
+                    }
+                    log.info(`Parsed maximumResults = ${maximumResults}`);
+                }
+                rawdata = $('li.jl');
+                json = rawdata
+                    .map(mapJobListItem)
+                    .get();
+            } catch (error) {
+                if (error.statusCode === 504 && limitRetries < 5) {
+                    log.info(' - Encountered rate limit, waiting 3 seconds');
+                    await Apify.utils.sleep(3000);
+                    limitRetries++;
+                    continue; // eslint-disable-line
+                } else {
+                    // Rethrow non rate-limit errors or if we are stuck
+                    throw error;
+                }
+            }
+
+            const itemsToSave = json.slice(0, maximumResults - savedItems);
+            searchResults.push(...itemsToSave);
+            savedItems += itemsToSave.length;
+            nextPageUrl = $('li.next a', '#FooterPageNav').attr('href');
+            log.info(`Page ${page}: Found ${itemsToSave.length} items, next page: ${nextPageUrl}`);
+            page++;
+        } while (nextPageUrl && savedItems < maximumResults);
+    }
+
+    if (input.category === 'Companies') {
+        do {
+            try {
+                const searchUrl = new URL(nextPageUrl, DOMAIN);
+                log.info(`GET ${searchUrl}`);
+                $ = await requestPromise({
+                    uri: searchUrl,
+                    ...optionsCheerio,
+                });
+                if (maximumResults < 0) {
+                    const cntStr = $('strong', 'div.count.margBot.floatLt.tightBot').last().text().replace(',', '');
+                    maximumResults = parseInt(cntStr, 10);
+                    if (!(maximumResults > 0)) {
+                        throw new Error(`Failed to parse companies count from ${cntStr}`);
+                    }
+                    log.info(`Parsed maximumResults = ${maximumResults}`);
+                }
+                rawdata = $('div.eiHdrModule');
+                json = rawdata
+                    .map(mapReviewListItem)
+                    .get();
+            } catch (error) {
+                if (error.statusCode === 504 && limitRetries < 5) {
+                    log.info(' - Encountered rate limit, waiting 3 seconds');
+                    await Apify.utils.sleep(3000);
+                    limitRetries++;
+                    continue; // eslint-disable-line
+                } else {
+                    // Rethrow non rate-limit errors or if we are stuck
+                    throw error;
+                }
+            }
+
+            const itemsToSave = json.slice(0, maximumResults - savedItems);
+            reviewResults.push(...itemsToSave);
+            savedItems += itemsToSave.length;
+            nextPageUrl = $('li.next a', '#FooterPageNav').attr('href');
+            log.info(`Page ${page}: Found ${itemsToSave.length} items, next page: ${nextPageUrl}`);
+            page++;
+        } while (nextPageUrl && savedItems < maximumResults);
+
+        // crawl company reviews to get jobs
+        const requestList = new Apify.RequestList({
+            sources: reviewResults.map(x => ({ url: DOMAIN + x.url, uniqueKey: x.id.toString() })),
+        });
+        await requestList.initialize();
+        const crawlerJobs = new Apify.BasicCrawler({
+            requestList,
+            handleRequestFunction: async ({ request }) => {
+                $ = await requestPromise({
+                    url: request.url,
+                    ...optionsCheerio,
+                });
+                const updatedItem = reviewResults.find(x => x.id === parseInt(request.uniqueKey, 10));
+                if (!updatedItem) {
+                    log.error(`- not found review listing id ${request.uniqueKey} in search results`);
+                    return;
+                }
+                const jobList = $('div.JobsListItemStyles__jobDetailsContainer').get();
+                log.info(`Preparing ${jobList.length} job(s) for company id ${request.uniqueKey}`);
+                for (const el of jobList) {
+                    const jobText = $('.JobDetailsStyles__jobTitle', el).text();
+                    const jobLink = $('.JobDetailsStyles__iconLink', el);
+                    if (!jobLink) {
+                        log.error('- no job link element');
+                        break;
+                    }
+                    const jlId = jobLink.data('id');
+                    // IMPORTANT - need one more call to get direct job link
+                    // jobLink.attr('href') leads to another job list with preselected item
+                    const listPageUrl = DOMAIN + jobLink.attr('href');
+                    log.info(`parsing ${listPageUrl}`);
+                    const page2 = await requestPromise({
+                        url: listPageUrl,
+                        ...optionsCheerio,
+                    });
+                    rawdata = page2('script[type="application/ld+json"]').html();
+                    json = JSON.parse(rawdata);
+                    if (!json && !json.itemListElement) {
+                        log.error('- jobs list not found!');
+                        break;
+                    }
+                    const jobItem = json.itemListElement[0];
+                    if (jobItem) {
+                        // transform to the same data object as we getting from job search results
+                        const jobResult = {
+                            ...updatedItem,
+                            id: jlId,
+                            url: jobItem.url,
+                            jobTitle: jobText,
+                        };
+                        searchResults.push(jobResult);
+                        log.info(`set to reparse from ${jobItem.url}`);
+                    } else {
+                        log.error('Job item not found!');
+                    }
+                }
+            },
+            handleFailedRequestFunction: async ({ request }) => {
+                await Apify.pushData({
+                    '#isFailed': true,
+                    '#debug': Apify.utils.createRequestDebugInfo(request),
+                });
+            },
+        });
+        await crawlerJobs.run();
+    }
 
     const checkUnique = [...new Set(searchResults.map(x => x.id))];
     log.info(`Found ${checkUnique.length} unique job listings out of ${searchResults.length} in total`);
@@ -152,15 +285,15 @@ Apify.main(async () => {
         throw new Error('No results from search!');
     }
 
-    const subPages = searchResults.map(x => ({ url: DOMAIN + x.url, uniqueKey: x.id.toString() }));
     const requestList = new Apify.RequestList({
-        sources: subPages,
+        sources: searchResults.map(x => ({ url: x.url, uniqueKey: x.id.toString() })),
     });
     await requestList.initialize();
 
     const crawler = new Apify.BasicCrawler({
         requestList,
         handleRequestFunction: async ({ request }) => {
+            log.info(`job GET ${request.url}`);
             $ = await requestPromise({
                 url: request.url,
                 ...optionsCheerio,
@@ -173,15 +306,38 @@ Apify.main(async () => {
                 log.error(`Not found job listing id ${request.uniqueKey} in search results`);
                 return;
             }
+
+            // remove html formatting from job description
+            let clearDetails = json.description;
+            try {
+                clearDetails = entities.decode(clearDetails); // this will transform html decoded content to plain html
+                clearDetails = $(clearDetails).text(); // then we create html from content and getting it as plain text
+            } catch (err) {
+                log.error(err);
+            }
+
+            // get company details from company overview page
+            const companyUrl = new URL($('div.logo.cell a').attr('href'), DOMAIN);
+            log.info(`overview GET ${companyUrl}`);
+            $ = await requestPromise({
+                url: companyUrl,
+                ...optionsCheerio,
+            });
+            const moreDetails = {};
+            $('div.infoEntity', '#EmpBasicInfo').each((i, el) => {
+                moreDetails[$('label', el).text()] = $('span', el).text().trim();
+            });
+
             log.info(`Saving details for job listing id ${request.uniqueKey}`);
             // unified output based on list item and sub page
+
             await Apify.pushData({
                 ...updatedItem,
                 url: json.url,
                 salary: json.estimatedSalary,
-                location: json.jobLocation,
-                companyDetails: json.hiringOrganization,
-                jobDetails: json.description,
+                jobLocation: json.jobLocation,
+                companyDetails: { ...json.hiringOrganization, ...moreDetails },
+                jobDetails: clearDetails,
             });
         },
         handleFailedRequestFunction: async ({ request }) => {
