3e5d08a49860f59d8c7eb968d5ab49a7d031f629
olegVeselov92 <oleg.veselov92@gmail.com>
2021-05-25 00:55:27 +0300

    rewrote to standart way, removed Company mode

--- src/consts.js
+++ src/consts.js
@@ -1,21 +1,23 @@
+/* eslint-disable linebreak-style */
 const BASE_URL = 'https://www.glassdoor.com';
 
-const REQUEST_HEADERS = {
-    headers: {
-        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.01',
-        'accept-encoding': 'gzip, deflate, sdch, br',
-        'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6',
-        referer: 'https://www.glassdoor.com/',
-        'upgrade-insecure-requests': '1',
-        // eslint-disable-next-line max-len
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
-        'Cache-Control': 'no-cache',
-        Connection: 'keep-alive',
-    },
-    gzip: true,
-};
+// AS WE ARE USING LATEST VERSION OF SDK (WITH GOT-SCRAPING) WE DON'T NEED ANY HEADERS (NEED TO CHECK)
+// const REQUEST_HEADERS = {
+//     headers: {
+//         accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.01',
+//         'accept-encoding': 'gzip, deflate, sdch, br',
+//         'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6',
+//         referer: 'https://www.glassdoor.com/',
+//         'upgrade-insecure-requests': '1',
+//         'user-agent':
+// 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
+//         'Cache-Control': 'no-cache',
+//         Connection: 'keep-alive',
+//     },
+//     gzip: true,
+// };
 
 module.exports = {
     BASE_URL,
-    REQUEST_HEADERS,
+    // REQUEST_HEADERS,
 };
